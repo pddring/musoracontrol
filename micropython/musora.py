37357b38f06f69f40ea0f33816902922fd19a93f
Pete Dring <dringp@fulford.york.sch.uk>
2023-11-01 16:39:32 +0000

    Added micropython

---
+++ micropython/musora.py
@@ -0,0 +1,58 @@
+import badger2040
+import jpegdec
+import time
+
+badger = badger2040.Badger2040()
+jpeg = jpegdec.JPEG(badger.display)
+
+badger.set_pen(15)
+badger.clear()
+
+# show images
+jpeg.open_file("/images/previous.jpg")
+jpeg.decode(40, 50)
+jpeg.open_file("/images/play.jpg")
+jpeg.decode(140, 50)
+jpeg.open_file("/images/next.jpg")
+jpeg.decode(230, 50)
+
+# show text
+badger.set_pen(0)
+badger.set_font("bitmap8")
+badger.text("Musora Controls", 70, 0)
+badger.text("Previous", 20, 100)
+badger.text("Play/Pause", 120, 100)
+badger.text("Next", 250, 100)
+badger.text("X", 280, 20)
+
+button_a = machine.Pin(badger2040.BUTTON_A, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_b = machine.Pin(badger2040.BUTTON_B, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_c = machine.Pin(badger2040.BUTTON_C, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_down = machine.Pin(badger2040.BUTTON_DOWN, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button_up = machine.Pin(badger2040.BUTTON_UP, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+
+badger.update()
+running = True
+while running:
+    # quit on up or down
+    if button_down.value() or button_up.value():
+        running = False
+    
+    if button_a.value():
+        print("previous")
+        while button_a.value():
+            time.sleep(0.1)
+    
+    if button_b.value():
+        print("play")
+        while button_b.value():
+            time.sleep(0.1)
+        
+    
+    if button_c.value():
+        print("next")
+        while button_c.value():
+            time.sleep(0.1)
+        
+    time.sleep(0.1)
